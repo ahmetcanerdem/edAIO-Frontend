b1584ef8d6adf468b185e7726ac3f5ed4bb20a09
ZehranurYilmaz <z.yilmaz@etu.edu.tr>
2022-03-06 23:23:15 +0300

    Birlestirme Basarıyla Tamamlandi

--- src/pages/ProfilPage.jsx
+++ src/pages/ProfilPage.jsx
@@ -2,53 +2,22 @@ import axios from "axios";
 import React, { useEffect,useState } from "react";
 
 function ProfilPage() {
-  const [data, setData] = useState(null);
+  const [profilBilgilerim, profilBilgilerimiAyarla] = useState(null);
 
   useEffect(() => {
-    axios.get("https://e8b0110b-ad1a-49c9-a7e4-7e295e79036f.mock.pstmn.io/students/grades")
+    axios.get("https://e8b0110b-ad1a-49c9-a7e4-7e295e79036f.mock.pstmn.io/students/students")
     .then(response => {
-      setData(response.data);
+      profilBilgilerimiAyarla(response.data);
     })
     .catch(error => {
       console.log(error);
     })
   },[]);
-  
-  const studentInfor = { //Example for write a page
-    idNumber: "001",
-    name: "Name 001",
-    surname: "Surname 001",
-  };
 
-  return (
-    <div className="profil-page">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">My Information</h1>
-            <div>
-              <span>Name:</span>
-              <span>{studentInfor.name}</span>
-            </div>
-            <div>
-              <span>Name:</span>
-              <span>{studentInfor.surname}</span>
-            </div>
-            <div>
-              <span>Data</span>
-              {!!data && <div>{data.gradesList[0].terms[1].courses[0].grade}</div>}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  return (<>
+    <h1>Bilgilerim Sayfası Güncelleniyor...</h1>
+    {profilBilgilerim ? (<div>Bilgilerim Backendden Doğru Bir Şekilde Alındı.</div>) : (<div>Burada küçük bir hata oldu.</div>)}
+    </>
   );
 }
 
