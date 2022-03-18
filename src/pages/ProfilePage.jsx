d0f0926f3641225232fa93efd72e07aa9f554354
Ahmed Taha SARI <ahmed.t.sari@gmail.com>
2022-03-18 22:36:14 +0300

    ders programi sinavlar

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,21 +1,94 @@
 import axios from "axios";
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack'
+import "../styles/ProfilePage.css";
 
 function ProfilPage() {
-  const [profilBilgilerim, profilBilgilerimiAyarla] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get("https://e8b0110b-ad1a-49c9-a7e4-7e295e79036f.mock.pstmn.io/students/profile")
-    .then(response => {
-      profilBilgilerimiAyarla(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  },[]);
+    axios.get("http://localhost:1337/profile")
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, []);
 
-  return (<>
-    <h1>Bilgilerim Sayfası Güncelleniyor...</h1>
+  return (
+    <>
+      <Stack direction="horizontal" gap={3}>
+        <h1>Bilgilerim Sayfası Güncelleniyor...</h1>
+        {!!data ? (
+          <div className="role">Role: {data.role}</div>
+        ) : (
+          <div></div>
+        )}
+      </Stack>
+      {!!data ? (
+        <div className="bg">
+
+          <Stack direction="horizontal" gap={1}>
+            <img src={data.image} className="masked" />
+            <Stack>
+              <Stack className="name" direction="horizontal" gap={3}>
+                <div>Name: </div>
+                <div className="ms-auto">{data.name}</div>
+              </Stack>
+              <Button className="btn-product" size="sm" href="#">{data.email}</Button>
+              <Stack className="department" direction="horizontal" gap={3}>
+                <div>Department: </div>
+                <div className="ms-auto">{data.department}</div>
+              </Stack>
+              <Stack className="id" direction="horizontal" gap={3}>
+                <div>Student number: </div>
+                <div className="ms-auto">{data.id}</div>
+              </Stack>
+            </Stack>
+            <Stack className="ls">
+              <h3 className="head">Major Info</h3>
+              <Stack direction="horizontal">
+                <Stack className="o-t">
+                  <h4>{data.faculty}</h4>
+                  <div>{data.department}</div>
+                </Stack>
+                <div className="o-o">Grade: {data.class} </div>
+                <div className="o-o">GPA: {data.gpa} </div>
+              </Stack>
+              <Stack className="sch" direction="horizontal" gap={3}>
+                <div>Scholarship: </div>
+                <div className="ms-auto">{data.scholarship}</div>
+              </Stack>
+              <Stack className="term" direction="horizontal" gap={3}>
+                <div>Term: </div>
+                <div className="ms-auto">{data.term}</div>
+              </Stack>
+              <Stack className="curr" direction="horizontal" gap={3}>
+                <div>Curriculum: </div>
+                <div className="ms-auto">{data.curriculum}</div>
+              </Stack>
+              <Stack className="enr" direction="horizontal" gap={3}>
+                <div>Enrolled in: </div>
+                <div className="ms-auto">{data.createdAt}</div>
+              </Stack>
+              <Stack className="stat" direction="horizontal" gap={3}>
+                <div>Status: </div>
+                <div className="ms-auto">{data.status}</div>
+              </Stack>
+              <Stack direction="horizontal">
+                <div className="w-w">Credits Taken: {data.creditTaken} </div>
+                <div className="w-w">Credits Completed: {data.creditCompleted} </div>
+              </Stack>
+            </Stack>
+          </Stack>
+        </div>
+      ) : (
+        <div>
+          Couldn't load data...
+        </div>
+      )}
     </>
   );
 }
