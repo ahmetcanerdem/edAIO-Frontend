0701a2eb6876791247afbfa9627872e1376a708f
ZehranurYilmaz <z.yilmaz@etu.edu.tr>
2022-03-27 15:42:46 +0300

    bug-fix

--- src/components/CustomTooltip.jsx
+++ src/components/CustomTooltip.jsx
@@ -2,31 +2,84 @@ import React from "react";
 import "../styles/CustomTooltip.css";
 
 const CustomTooltip = (props) => {
-  const lessonIndex = props.value.indexOf("Derslik");
-  const lessonName = props.value.substring(0, lessonIndex);
+  const lessonInfo = [];
+  const lecture = [];
+
   let lessonClass;
   let zoomId;
-  const loadingId = props.value.indexOf("ID ");
+  let lessonIndex;
+  let lessonName;
+
+  if (props.value instanceof Array) {
+    props.value.forEach((item) => {
+      lecture.push(item);
+    });
+  } else {
+    lecture.push(props.value);
+  }
+
+  lecture.forEach((item) => {
+    lessonIndex = item.indexOf("Derslik");
+    lessonName = item.substring(0, lessonIndex);
+    const loadingId = item.indexOf("ID ");
     if (loadingId > 0) {
-      lessonClass = props.value.substring(lessonIndex + 9, loadingId - 1);
-      zoomId = props.value.substring(loadingId);
+      lessonClass = item.substring(lessonIndex + 9, loadingId - 1);
+      zoomId = item.substring(loadingId);
     } else {
-      const zoomIndex = props.value.indexOf("Zoom");
-      zoomId = props.value.substring(zoomIndex + 9);
-      lessonClass = props.value.substring(lessonIndex + 9, zoomIndex - 1);
+      const zoomIndex = item.indexOf("Zoom");
+      zoomId = item.substring(zoomIndex + 9);
+      lessonClass = item.substring(lessonIndex + 9, zoomIndex - 1);
     }
+    lessonInfo.push({
+      lessonName: lessonName,
+      lessonClass: lessonClass,
+      zoomId: zoomId,
+    });
+  });
+
+  const heightSize = lessonInfo.length * 80 + "px";
+
+  const shownItem = () => {
+    const items = [];
+    lessonInfo.forEach((lesson) => {
+      items.push(
+        <div className="element-tooltip">
+          <div>
+            <span className="header-tooltip">Ders İsmi: </span> {lesson.lessonName}
+          </div>
+          <div>
+            <span className="header-tooltip">Ders Saati: </span> {props.data.hours}
+          </div>
+          <div>
+            <span className="header-tooltip">Derslik: </span>
+            {lesson.lessonClass}
+          </div>
+          <div>
+            <span className="header-tooltip">Zoom ID: </span>
+            {lesson.zoomId}
+          </div>
+        </div>
+      );
+    });
+    return <>{items}</>;
+  };
 
   return (
     <div
       className="custom-tooltip"
-      style={{ backgroundColor: props.color || "white" }}
+      style={{
+        backgroundColor: props.color || "white",
+        height: heightSize,
+      }}
     >
-      <p>
-        <div>Ders İsmi: {lessonName}</div>
-        <div>Ders Saati: {props.data.hours}</div>
-        <div>Derslik: {lessonClass}</div>
-        <div>Zoom ID: {zoomId}</div>
-      </p>
+      <div
+        style={{
+          height: heightSize,
+          backgroundColor: props.color || "white",
+        }}
+      >
+        {shownItem()}
+      </div>
     </div>
   );
 };
