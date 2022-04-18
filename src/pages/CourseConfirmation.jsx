ffd5f59a174258355d462cc46060e1f6936ed6a5
ZehranurYilmaz <z.yilmaz@etu.edu.tr>
2022-04-18 13:30:35 +0300

    Course Selection Linked okii

--- src/pages/CourseConfirmation.jsx
+++ src/pages/CourseConfirmation.jsx
@@ -1,15 +1,15 @@
 import { useLocation } from "react-router-dom";
 import { React, useEffect, useState } from "react";
-import { Form, Row } from "react-bootstrap";
+import { Form, Row, Col, Nav } from "react-bootstrap";
 import "../styles/Buttons.css";
 import axios from "axios";
 
 const unselectedCourses = [];
 var courses = [];
 function CourseConfirmation() {
-	const studentId = JSON.parse(localStorage.getItem("userData")).id;
+  const studentId = JSON.parse(localStorage.getItem("userData")).id;
   const location = useLocation();
-	const [confirmed, setConfirmed] = useState(false);
+  const [confirmed, setConfirmed] = useState(false);
   const selectedCourses = location.state;
   const server = "http://localhost:5000";
 
@@ -19,27 +19,24 @@ function CourseConfirmation() {
     }
   }
 
-
   const handleConfirmation = (e) => {
-		if(e.target.checked){
-			courses = [];
-    	setConfirmed(true);
-			for(let i = 0; i < selectedCourses.length; i++){
-				if(unselectedCourses[i] === false && i % 2 === 0){
-					courses.push(selectedCourses[i]);
-				}
-			}
-      console.log(courses)
-			axios({
-				method: "post",
-				url: server + "/student/addCourse/sid=" + studentId,
-				data: {
-					course: courses
-				},
-			});
-		}
-		else
-			setConfirmed(false);
+    if (e.target.checked) {
+      courses = [];
+      setConfirmed(true);
+      for (let i = 0; i < selectedCourses.length; i++) {
+        if (unselectedCourses[i] === false && i % 2 === 0) {
+          courses.push(selectedCourses[i]);
+        }
+      }
+      console.log(courses);
+      axios({
+        method: "post",
+        url: server + "/student/addCourse/sid=" + studentId,
+        data: {
+          course: courses,
+        },
+      });
+    } else setConfirmed(false);
   };
 
   const handleDeletion = (e) => {
@@ -54,6 +51,14 @@ function CourseConfirmation() {
   //list users for selection
   return (
     <div>
+      <h1>Ders Onayı</h1>
+      <Col>
+        <Nav.Link className="button button-4" href="/courseSelection">
+          <h3 style ={{
+            padding: "10px"
+          }}>{" "} Ders Ekleme Ekranına Geri Dön{" "}</h3>
+        </Nav.Link>
+      </Col>
       <Form>
         <Form.Check
           type="switch"
@@ -68,7 +73,7 @@ function CourseConfirmation() {
           label="Danisman Onayi"
         />
       </Form>
-			<Row> </Row>
+      <Row> </Row>
       {!confirmed && !!selectedCourses && (
         <>
           {selectedCourses.map((user, index) => {
@@ -96,7 +101,7 @@ function CourseConfirmation() {
           })}
         </>
       )}
-			{confirmed && !!selectedCourses && (
+      {confirmed && !!selectedCourses && (
         <>
           {selectedCourses.map((user, index) => {
             return (
@@ -108,7 +113,7 @@ function CourseConfirmation() {
                     <ul>
                       <li key={index}>
                         <input
-													disabled
+                          disabled
                           type="checkbox"
                           id={index}
                           defaultChecked={!unselectedCourses[index]}
